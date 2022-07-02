74cf817a16ed8e63b06c3874c4b71af3a16dcbe1
Stepalom77 <stepalom22@gmail.com>
2022-07-02 14:07:16 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Server up & running ✅');
+  })
+
+app.listen(PORT, () => console.log(`Listening on port:${PORT}`))
