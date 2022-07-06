d2f1871799f13da64c54ac4baa485beba28ab62e
Stepalom77 <stepalom22@gmail.com>
2022-07-05 22:42:35 -0500

    packages added

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const router = require('./server/routes');
+
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use('/api/v1', router);
 
 app.get('/', (req, res) => {
     res.send('Server up & running ✅');
   })
+
+
 
 app.listen(PORT, () => console.log(`Listening on port:${PORT}`))
