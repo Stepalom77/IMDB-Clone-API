bb8db8315de3595f8584d3770331ecfae7de2de4
Stepalom77 <stepalom22@gmail.com>
2022-07-24 20:18:26 -0500

    Procfile and heroku configuration

--- server/models/config/config.js
+++ server/models/config/config.js
@@ -15,10 +15,7 @@ test: {
     dialect: "postgres"
 },
 production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: "postgres"
+    use_env_variable: "DATABASE_URL",
+    logging: false
 }
 };
