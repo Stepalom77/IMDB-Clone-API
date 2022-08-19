c8f00ef6daa7538b84d70a403fe4a33dc9d850e7
Stepalom77 <stepalom22@gmail.com>
2022-08-19 16:55:44 -0500

    swagger and route of docs added

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 require('dotenv').config()
 const express = require('express');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Clone of The Internet Movie Database API",
+			version: "1.0.0",
+			description: "Creating a API Clone of The Internet Movie Database",
+		},
+		servers: [
+			{
+				url: "http://localhost:5000",
+        description: "Local server"
+			},
+      {
+        url: "https://ste-imdb-clone.herokuapp.com/",
+        description: "Production server"
+      }
+		],
+	},
+	apis: ["./server/routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
 //Import routes
 const crewMembersRoute = require('./server/routes/crewMembersRoutes');
 const genresRoute = require('./server/routes/genresRoutes');
@@ -19,7 +43,6 @@ const usersRoute = require('./server/routes/usersRoutes');
 
 //Middlewares
 app.use(express.urlencoded({extended: true}));
-app.use(bodyParser.json({ type: 'application/json' }));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
@@ -33,6 +56,7 @@ app.use('/api/v1', rolesRoute);
 app.use('/api/v1', tvEpisodesRoute);
 app.use('/api/v1', tvSeriesRoute);
 app.use('/api/v1', usersRoute);
+app.use('/api/v1/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
 
 app.get('/', (req, res) => {
     res.send('Server up & running ✅');
