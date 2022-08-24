1589b1f9028598b6511f5b37bf22b38a92fe6f59
Stephano Carlo Palomino Madueño <86162750+Stepalom77@users.noreply.github.com>
2022-08-23 20:09:55 -0400

    Swaggerhub (#1)
    * Push from SwaggerHub.
    
    * API documentation with swagger setup
    
    * API documentation completed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require("cors");
 const swaggerUI = require("swagger-ui-express");
-const docs = require('./docs')
+const docs = require('./docs/swagger.json')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
