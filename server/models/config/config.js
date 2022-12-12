882ac0a05a833bf636945dbeb86996aefbcadb2e
Stepalom77 <stepalom22@gmail.com>
2022-12-11 20:50:52 -0500

    sequelize fix

--- server/models/config/config.js
+++ server/models/config/config.js
@@ -20,6 +20,7 @@ production: {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres"
 }
 /*
